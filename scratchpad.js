72bb8e066bef41d6dfb27476a920ed9e49687d8f
Kevin Mas Ruiz <masruizkevin@gmail.com>
2016-05-02 12:36:45 +0200

    sampling

--- scratchpad.js
+++ scratchpad.js
@@ -1,3 +1,7 @@
-function doSomething() {
-    return  Rx.Observable.from([1, 2, 3, 4, 5, 6, 7, 8]);
+function numbers() {
+    return Rx.Observable.from([1, 2, 3, 4, 5, 6, 7, 8]);
+}
+
+function x10(number) {
+    return Rx.Observable.just(number * 10);
 }
